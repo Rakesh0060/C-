75d9064d0a15772f5b4d3d3a4ae03a32b29f0886
Rakesh0060 <112700200+Rakesh0060@users.noreply.github.com>
2022-09-20 10:52:25 +0530

    Create 3.reverse.cpp

---
+++ 3.reverse.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void getreverse(string &str, int i)
+{
+
+    if (i > (str.length() - 1 - i))
+
+    {
+
+        return;
+
+    }
+
+    swap(str[i], str[str.length() - i - 1]);
+
+    i++;
+
+    getreverse(str, i);
+}
+
+int main()
+{
+
+    string name = "geeksforgeeks";
+ 
+
+    getreverse(name, 0);
+
+    cout << name << endl;
+
+    return 0;
+}
